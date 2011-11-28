887c442512484b8ff78beffa76c42319e5d765cd
hzeus <heiko.zeus@zweitag.de>
2011-11-28 11:43:09 +0100

    Moves tabs helpers to single helper module

--- app/helpers/bootstrap_helper.rb
+++ app/helpers/bootstrap_helper.rb
@@ -35,5 +35,20 @@ module BootstrapHelper
     end
   end
 
+  def bootstrap_tabs(opts = {}, &block)
+    tabs_or_pills(opts.merge(:type => :tabs), &block)
+  end
+
+  def bootstrap_pills(opts = {}, &block)
+    tabs_or_pills(opts.merge(:type => :pills), &block)
+  end
+
+  private
+
+  def tabs_or_pills(opts = {}, &block)
+    BootstrapRenderer::Tabs.new(opts, self).tap do |renderer| 
+      yield renderer
+    end
+  end
 
 end
