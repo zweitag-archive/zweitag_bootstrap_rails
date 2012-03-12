35ad00015bb3f4c81b05473c3cb3781fcc400482
Heiko Zeus <heiko.zeus@zweitag.de>
2012-03-12 13:22:28 +0100

    Removes deprecated simple_form support

--- lib/zweitag_bootstrap_rails/engine.rb
+++ lib/zweitag_bootstrap_rails/engine.rb
@@ -3,44 +3,6 @@ require 'bootstrap_renderer'
 module ZweitagBootstrapRails
   class Engine < Rails::Engine
 
-    # https://github.com/plataformatec/simple_form/wiki/Twitter-Bootstrap-integration
-    initializer "simple_form_bootstrap" do
-
-      if defined?(SimpleForm)
-
-        SimpleForm.setup do |config|
-
-          # CSS class used on errors.
-          config.error_class = :"help-inline"
-
-          # CSS class to add for error notification helper.
-          config.error_notification_class = "alert-message error"
-
-          # CSS class to add to all wrapper tags.
-          config.wrapper_class = :clearfix
-
-          # CSS class to add to the wrapper if the field has errors.
-          config.wrapper_error_class = :error
-
-          # You can wrap a collection of radio/check boxes in a pre-defined tag, defaulting to none.
-          config.collection_wrapper_tag = "ul class='inputs-list'"
-
-          # You can wrap each item in a collection of radio/check boxes with a tag, defaulting to span.
-          config.item_wrapper_tag = "li"
-
-          # You can define the class to use on all forms. Default is simple_form.
-          config.form_class = nil
-
-          # Tell browsers whether to use default HTML5 validations (novalidate option).
-          # Default is enabled.
-          config.browser_validations = false
-
-        end
-
-      end
-    end 
-
-
     initializer "will_paginate_bootstrap" do
       # https://gist.github.com/1214011
       if defined?(WillPaginate)
